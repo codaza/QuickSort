3f84a1924888312422c2e17af2c82a44ebdd72f0
codaza <84292305+codaza@users.noreply.github.com>
2021-08-20 12:17:14 +0300

    Initial commit

---
+++ src/QuickSortAlgorithm/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace QuickSortAlgorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = { 9, 12, 9, 2, 17, 1, 6 };
+
+            int[] sortedArray = QuickSort(inputArray, 0, inputArray.Length - 1);
+
+            Console.WriteLine($"Sorted array: {string.Join(", ", sortedArray)}");
+
+            Console.ReadLine();
+        }
+
+        private static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+            QuickSort(array, minIndex, pivotIndex - 1);
+
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        private static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+        {
+            int pivot = minIndex - 1;
+
+            for (int i = minIndex; i <= maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+
+            return pivot;
+        }
+
+        private static void Swap(ref int leftItem, ref int rightItem)
+        {
+            int temp = leftItem;
+
+            leftItem = rightItem;
+
+            rightItem = temp;
+        }
+    }
+}
